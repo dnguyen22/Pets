73d7f45c4b38a3932a310aa572e736984ed0b6d3
dnguyen22 <don.nguyen22@gmail.com>
2017-06-25 16:24:25 -0500

    Add PetProvider class
    Add class PetProvider to abstract interactions with database.

---
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -0,0 +1,101 @@
+package com.example.android.pets.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Don on 6/25/2017.
+ */
+
+/**
+ * {@link ContentProvider} for Pets app.
+ */
+public class PetProvider extends ContentProvider {
+
+    /** Tag for the log messages */
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    /** Database helper that will provide us access to the database */
+    private PetDbHelper mDbHelper;
+
+    /**
+     * Initialize the provider and the database helper object.
+     * @return
+     */
+    @Override
+    public boolean onCreate() {
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        mDbHelper = new PetDbHelper(getContext());
+        return true;
+    }
+
+    /**
+     * Perform the query for the given URI. Use the given projection, selection, selection arguments, and the sort order.
+     * @param uri query URI
+     * @param projection String array of columns for query
+     * @param selection String for SQL "where" clause
+     * @param selectionArgs String array for SQL "where" clause arguments
+     * @param sortOrder String for SQL sort order of query results.
+     * @return
+     */
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    /**
+     * Returns the MIME type of data for the content URI.
+     * @param uri
+     * @return
+     */
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    /**
+     * Insert new data into the provider with the given ContentValues.
+     * @param uri
+     * @param contentValues
+     * @return
+     */
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    /**
+     * Delete the data at the given selection and selection arguments.
+     * @param uri
+     * @param s
+     * @param strings
+     * @return
+     */
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    /**
+     * Updates the data at the given selection and selection arguments, with the new ContentValues
+     * @param uri
+     * @param contentValues
+     * @param s
+     * @param strings
+     * @return
+     */
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
+
+
